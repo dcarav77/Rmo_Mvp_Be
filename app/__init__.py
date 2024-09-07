1c04ade106276137546867fb02f48803361aac85
dcarav77 <dcarav77@gmail.com>
2024-09-07 15:30:48 +0200

    added cores to init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS 
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -11,6 +12,7 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
+    CORS(app) 
 
     # Only import routes after app is fully initialized
     with app.app_context():
