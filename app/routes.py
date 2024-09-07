b285ce21a2b9bb1b7da969a5b5e547abe9253268
dcarav77 <dcarav77@gmail.com>
2024-09-07 15:31:45 +0200

    added home route for local host 3000

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,11 @@ from app import db
 from app.models import TechnicalObject
 
 def init_app(app):
+   
+    @app.route('/', methods=['GET'])
+    def home():
+        return jsonify({"message": "Welcome to the Flask API!"})
+    
     @app.route('/technical_objects', methods=['GET'])
     def get_technical_objects():
         objects = TechnicalObject.query.all()
