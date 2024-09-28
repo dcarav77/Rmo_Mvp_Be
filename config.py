4678062324bdd1138d6d35409b678778dc4e1a6d
dcarav77 <dcarav77@gmail.com>
2024-09-28 15:56:38 +0200

    deleted config will add back

--- config.py
+++
@@ -1,6 +0,0 @@
-# import os
-
-# class Config:
-#     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://dcarav77:bamsam84@localhost/aa_rmo'
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-#     SECRET_KEY = os.urandom(24)
